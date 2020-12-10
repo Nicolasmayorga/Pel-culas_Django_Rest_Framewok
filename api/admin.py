ffad3c8cec2b2e5d000401de804a4d82c13243e5
Nicolasmayorga <nmayorgav@gmail.com>
2020-12-10 03:34:45 -0500

    In this commit we create the module of administration of cvs files to allow to the administrator import and export the model and the database into the graphic interface

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import PeliculaFavorita
 
 # Register your models here.
+
+
+@admin.register(PeliculaFavorita)
+class PeliculaFavoritaAdmin(ImportExportModelAdmin):
+    pass
